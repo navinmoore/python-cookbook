ffa0983505d04d9ac6d411caeb5f34e9874aeb8d
wangzhiyao <258641620@qq.com>
2018-01-27 18:50:29 +0800

    all

---
+++ Class/StackCode.py
@@ -0,0 +1,47 @@
+# -*-coding:utf-8-*-
+
+class NodeVisitor:
+    def __init__(self, node):
+        methname = 'visit_' + type(node).__name__
+        meth = getattr(self, methname, None)
+        if meth is None:
+            meth = self.generic_visit
+        return meth(node)
+
+    def generic_visit(self, node):
+        raise RuntimeError("No () method".format('visit_' + type(node).__name__))
+
+
+class StackCode(NodeVisitor):
+    def generate_code(self, node):
+        self.instructions = []
+        self.visit(node)
+        return self.instructions
+
+    def visit_Number(self, node):
+        self.instructions.append(('PUSH', node.value))
+
+    def binop(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+        self.instructions.append((instruction,))
+
+    def visit_Add(self, node):
+        self.binop(node, 'ADD')
+
+    def visit_Sub(self, node):
+        self.binop(node, 'SUB')
+
+    def visit_Mul(self, node):
+        self.binop(node, 'MUL')
+
+    def visit_Div(self, node):
+        self.binop(node, 'DIV')
+
+    def unaryop(self, node, instruction):
+        self.visit(node.operand)
+        self.instructions.append((instruction))
+
+    def visit_Negate(self, node):
+        self.unaryop(node, 'NEG')
+
